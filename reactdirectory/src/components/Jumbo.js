ba5b7811b21e11da814b57204df102648eadcda5
Slice Wess <ernestwesson86@gmail.com>
2020-11-16 18:02:39 -0800

    issues with react jumbotron

--- reactdirectory/src/components/Jumbo.js
+++ reactdirectory/src/components/Jumbo.js
@@ -1,10 +1,16 @@
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
 
-import Jumbotron from 'react-bootstrap/Jumbotron'
+<Jumbotron fluid>
+  <Container>
+    <h1>Fluid jumbotron</h1>
+    <p>
+      This is a modified jumbotron that occupies the entire horizontal space of
+      its parent.
+    </p>
+  </Container>
+</Jumbotron>
 
-<div className="jumbotron jumbotron-fluid">
-  <div className="container">
-    <h1 className="display-4">Fluid jumbotron</h1>
-    <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-  </div>
-</div>
 export default Jumbotron;
+export default Container;
