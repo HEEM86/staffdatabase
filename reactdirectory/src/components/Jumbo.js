036c97d91a63b0867949e8ee17fcacc680c4a58d
Slice Wess <ernestwesson86@gmail.com>
2020-11-23 17:35:22 -0800

    updated jumbotron

--- reactdirectory/src/components/Jumbo.js
+++ reactdirectory/src/components/Jumbo.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import{} Container from 'react-bootstrap/Container';
+import React, { Component } from 'react';
+import { Jumbotron, Container } from 'react-bootstrap';
 
-<Jumbotron fluid>
-  <Container>
-    <h1>Fluid jumbotron</h1>
-    <p>
-      This is a modified jumbotron that occupies the entire horizontal space of
-      its parent.
-    </p>
-  </Container>
-</Jumbotron>
 
-export default [Jumbotron, Container];
+
+class Jumbo extends Component{
+  render() {
+    return(
+      <Jumbotron fluid>
+        <Container>
+          <h1>Fluid jumbotron</h1>
+          <p>
+            This is a modified jumbotron that occupies the entire horizontal space of
+            its parent.
+          </p>
+        </Container>
+      </Jumbotron>
+    );
+  }
+
+}
+
+export default Jumbo;
