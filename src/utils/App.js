abdf9a4efff77b562c4028156388b942fc1815be
Slice Wess <ernestwesson86@gmail.com>
2020-12-05 14:26:29 -0800

    saving again

--- src/utils/App.js
+++ src/utils/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import EmployeeDirectory from "./components/directory";
-import SearchForm from "./components/SearchForm";
-import Jumbo from "./components/Jumbo";
+import EmployeeDirectory from "./components/EmployeeContainer";
+import SearchForm from "../components/SearchForm";
+import Jumbo from "./components/ResultList";
 
 function App() {
   return (
