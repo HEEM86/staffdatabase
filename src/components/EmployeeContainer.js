9a9da5f48d4dc6fa967233038f1deed4fbac8c94
Slice Wess <ernestwesson86@gmail.com>
2020-12-05 19:04:55 -0800

    working on Employee Container file

--- src/components/EmployeeContainer.js
+++ src/components/EmployeeContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-// import SearchForm from "./SearchForm.js";
-// import ResultList from "./ResultList";
-// import API from "../utils/API";
+import SearchForm from "./SearchForm.js";
+import ResultList from "./ResultList";
+import API from "../utils/API";
 
 class EmployeeContainer extends Component {
   state = {
@@ -73,3 +73,74 @@ comparison =-1;
 return comparison;
 });
 break;
+case "Email":
+tempResult=tempResult.sort((a,b)=>{
+let comparison =0;
+if(a.email>b.email){
+    comparison= 1;
+}else if (a.email<b.email){
+    comparison =-1;
+}
+return comparison;
+});
+break;
+case "Phone":
+tempResult=tempResult.sort((a,b)=>{
+let comparison =0;
+if(a.phone>b.phone){
+    comparison= 1;
+}else if (a.phone<b.phone){
+    comparison =-1;
+}
+return comparison;
+});
+break;
+    case "DOB":
+        tempResult = tempResult.sort((a, b) => {
+            let comparison = 0;
+            if (a.dob > b.dob) {
+                comparison = 1;
+            } else if (a.dob < b.dob) {
+                comparison = -1;
+            }
+            return comparison;
+        });
+        break;
+    default:
+        break;
+
+}
+this.setState({queryResults:tempResult});
+
+}
+
+handleInputChange = event => {
+    const value = event.target.value;
+    this.setState({
+      search: value
+    },()=>{this.searchEmployees(this.state.search);});
+    
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="jumbotron">
+            <h1>Employee Directory</h1>
+
+        </div>
+        <SearchForm
+          search={this.state.search}
+          handleInputChange={this.handleInputChange}
+        />
+        <ResultList 
+            results={this.state.queryResults}
+            handleSort={this.handleSort}    
+        />
+
+      </div>
+    );
+  }
+}
+
+export default EmployeeContainer;
--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -10,8 +10,26 @@ function ResultList(props) {
             <tr>
                 <th scope="col">Image</th>
                 <th scope="col">Name<i className=" btn fa fa-fw fa-sort" onClick={()=>props.handleSort("Name")}></i></th>
-                <th scope="col">Phone<i className="fa fa-fw fa-sort" onClick={()=>props.handleSort("Phone")}></i></th>
+                {/* <th scope="col">Phone<i className="fa fa-fw fa-sort" onClick={()=>props.handleSort("Phone")}></i></th>
                 <th scope="col">Email<i className="fa fa-fw fa-sort" onClick={()=>props.handleSort("Email")}></i></th>
-                <th scope="col">DOB<i className="fa fa-fw fa-sort" onClick={()=>props.handleSort("DOB")}></i></th>
+                <th scope="col">DOB<i className="fa fa-fw fa-sort" onClick={()=>props.handleSort("DOB")}></i></th> */}
             </tr>
         </thead>
+
+    <tbody>
+        {props.results.map(result => (
+            <tr key={result.id}>
+                <td><img alt={result.name} className="img-fluid" src={result.picture} /></td>
+                <td>{result.name}</td>
+                <td>{result.phone}</td>
+                <td>{result.email}</td>
+                <td><Moment format="MM/DD/YYYY" date={result.dob}/></td>
+            </tr>
+        ))}
+    </tbody>
+    
+</table>
+);
+}
+
+export default ResultList;
